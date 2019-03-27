6e45b284110d239018c9d9bda36f62108c295911
Cristopher Barrios <cjrbarrios@gmail.com>
2019-03-27 09:16:14 -0600

    Segundo

--- app/src/main/java/com/example/labo2cristopherbarrios/MainActivity.kt
+++ app/src/main/java/com/example/labo2cristopherbarrios/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.labo2cristopherbarrios
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun button(view: View) {
+        val hola = Intent(this@MainActivity, Main2Activity::class.java)
+        startActivity(hola)
     }
 }
