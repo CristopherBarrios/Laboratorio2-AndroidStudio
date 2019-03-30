97c0642e86b1cf15d6b45cb21fc095fb1b542163
Cristopher Barrios <cjrbarrios@gmail.com>
2019-03-29 23:35:53 -0600

    suma

--- app/src/main/java/com/example/labo2cristopherbarrios/Main2Activity.kt
+++ app/src/main/java/com/example/labo2cristopherbarrios/Main2Activity.kt
@@ -4,12 +4,26 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
+    var  contador= 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+    }
+    fun suma(view: View) {
+        contador++
+        val txt = findViewById(R.id.textView) as TextView
+        txt.setText(contador)
+    }
+    fun resta(view: View) {
+        contador--
+        val txt = findViewById(R.id.textView) as TextView
+        txt.setText(contador)
     }
 
 }
