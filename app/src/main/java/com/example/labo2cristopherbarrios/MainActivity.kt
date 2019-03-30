0d9e8f5c78c085c9178b9e22899cd95aceb6283d
Cristopher Barrios <cjrbarrios@gmail.com>
2019-03-30 14:42:18 -0600

    Final

--- app/src/main/java/com/example/labo2cristopherbarrios/MainActivity.kt
+++ app/src/main/java/com/example/labo2cristopherbarrios/MainActivity.kt
@@ -5,15 +5,28 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    var lista = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lista.add("5")
+        lista.add("6")
+        lista.add("13")
+        lista.add("25")
+        lista.add("9")
+
+        val listView = findViewById<ListView>(R.id.listata)
+        listView.adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,lista)
     }
     fun ventana(view: View) {
         val hola = Intent(this@MainActivity, Main2Activity::class.java)
@@ -23,4 +36,6 @@ class MainActivity : AppCompatActivity() {
     fun ventana2(view: View) {
         Toast.makeText(this,"Se eliminó el historial",Toast.LENGTH_LONG).show()
     }
+    
+    
 }
